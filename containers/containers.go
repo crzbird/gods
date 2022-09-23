db5a45ab1f80d0906937d1b4af02750ed1fa443f
丁澄宇 <dingchengyu@hydee.cn>
2022-09-23 17:02:50 +0800

    arrayList 存储泛型指针

--- containers/containers.go
+++ containers/containers.go
@@ -13,20 +13,22 @@
 // Serialization provides serializers (marshalers) and deserializers (unmarshalers).
 package containers
 
-import "github.com/emirpasic/gods/utils"
+import (
+	"github.com/emirpasic/gods/utils"
+)
 
 // Container is base interface that all data structures implement.
-type Container interface {
+type Container[T any] interface {
 	Empty() bool
 	Size() int
 	Clear()
-	Values() []interface{}
+	Values() []any
 	String() string
 }
 
 // GetSortedValues returns sorted container's elements with respect to the passed comparator.
 // Does not affect the ordering of elements within the container.
-func GetSortedValues(container Container, comparator utils.Comparator) []interface{} {
+func GetSortedValues(container Container[any], comparator utils.Comparator) []any {
 	values := container.Values()
 	if len(values) < 2 {
 		return values
